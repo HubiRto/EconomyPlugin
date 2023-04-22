327243f8c491ff742f34caa19dcc3427eb7a5695
hubert <hubert.rybicki.hr@gmail.com>
2023-04-22 18:33:30 +0200

    Init

---
+++ src/main/java/pl/pomoku/economyplugin/EconomyPlugin.java
@@ -0,0 +1,17 @@
+package pl.pomoku.economyplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EconomyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/pl/pomoku/economyplugin/entity/Balance.java
@@ -0,0 +1,4 @@
+package pl.pomoku.economyplugin.entity;
+
+public class Balance {
+}
