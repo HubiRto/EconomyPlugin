8268a745db5c263959eaf71cb6c3b7887f78efee
Hubert Rybicki <hubert.rybicki.hr@gmail.com>
2023-05-03 21:16:53 +0200

    Dodanie dockera

---
+++ src/main/java/pl/pomoku/economyplugin/command/Withdraw.java
@@ -0,0 +1,21 @@
+package pl.pomoku.economyplugin.command;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import pl.pomoku.pomokupluginsrepository.commands.CommandInfo;
+import pl.pomoku.pomokupluginsrepository.commands.EasyCommand;
+
+@CommandInfo(name = "withdraw", requiresPlayer = true)
+public class Withdraw extends EasyCommand {
+    @Override
+    public void execute(Player p, String[] args) {
+
+    }
+
+    private static boolean isPlayerHaveFreeSlot(Player player){
+        Inventory inventory = player.getInventory();
+        for(int i = 0; i < inventory.getSize(); i++){
+        }
+    }
+}
